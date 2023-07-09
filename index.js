ed35d69b0e9585afb08c77603191c3a4702faabb
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-09 20:41:55 +0300

    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const divide = (num1 = 2000, num2 = 100) => {
+  return num1 / num2;
+}
+divide();
+
+const square = (x) => {
+  return x * x;
+}
+square();
+
+const add = (a, b) => {
+  return a + b;
+}
+add();
